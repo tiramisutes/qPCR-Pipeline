6bdd7aefd30620e96da978fd27ede8cfac12c34d
hope <1099808298@qq.com>
2019-02-28 16:26:17 +0800

    Update qPCR.sh

--- qPCR.sh
+++ qPCR.sh
@@ -3,3 +3,4 @@ Rscript qPCR.R $1/$2 ve
 python qPCR.py ${2%.*}_ubq7.csv ${2%.*}_ve.csv
 cat ${2%.*}_R.csv | awk -F"," 'BEGIN{OFS=","} {print $0,$1}' | sed "s/-[0-9]$//g" | sed "s/Sample/Group/2" > Pro_${2%.*}_R.csv
 Rscript plot_qPCR.R Pro_${2%.*}_R.csv $3 $4 $5
+Rscript qPCR_ANOVA.R ${2%.*}_ANOVA.csv $6
