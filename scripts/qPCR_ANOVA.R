567f401454b9a539851231076160c62c80d00324
hope <1099808298@qq.com>
2019-02-28 20:15:15 +0800

    Update qPCR_ANOVA.R

--- scripts/qPCR_ANOVA.R
+++ scripts/qPCR_ANOVA.R
@@ -4,11 +4,12 @@ if (length(args)==0) {
   stop("At least one argument must be supplied (input file).n", call.=FALSE)
 }
 
-par(mai=c(0.5,1.5,0,0)); #set margin for plot bottom,left top, right
+par(mar=c(5,10,4,1)+.1); #set margin for plot bottom,left top, right
 library(multcomp)
 strains<-read.csv(args[1],header=TRUE)
 strains$Sample = relevel(strains$Sample,args[2])
 test<-summary(glht(aov(X2log ~ Sample, data=strains), linfct=mcp(Sample="Dunnett")))
+test
 pdf(paste0(gsub("ANOVA\\.csv","T-test",args[1]),".pdf"),width=8, height=6,useDingbats=FALSE)
 plot(glht(aov(X2log ~ Sample, data=strains), linfct=mcp(Sample="Dunnett")))
 dev.off()
